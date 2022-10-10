bab466d1a99ec05ff95796c8a84f98623ed38fdc
Dorkom <varerik95@gmail.com>
2022-10-10 18:16:35 -0500

    routes

---
+++ reactproyecto/src/components/layout/Layout.jsx
@@ -0,0 +1,10 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+const Layout = (props) => {
+
+    return (<Container></Container>)
+}
+
+export default Layout
---
+++ reactproyecto/src/components/pages/HomePage.jsx
@@ -0,0 +1,9 @@
+const HomePage = () => {
+    return (
+        <>
+        <Layout main={<Home />}></Layout>
+        </>
+    )
+}
+
+export default HomePage
