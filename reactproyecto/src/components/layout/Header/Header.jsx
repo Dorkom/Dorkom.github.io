1f50055b5a3785edc40ef46036f9213c1b9f67fe
Dorkom <varerik95@gmail.com>
2022-10-11 19:32:30 -0500

    search1

--- reactproyecto/src/components/layout/Header/Header.jsx
+++ reactproyecto/src/components/layout/Header/Header.jsx
@@ -5,7 +5,26 @@ import Nav from 'react-bootstrap/Nav';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 
+
+
+
+import React, { useState } from 'react';
+import { Typeahead } from 'react-bootstrap-typeahead'; // ES2015
+// var Typeahead = require('react-bootstrap-typeahead').Typeahead;
+import options from './data';
+<link
+  rel="stylesheet"
+  href="https://unpkg.com/react-bootstrap-typeahead/css/Typeahead.css"
+/>
+
+
+
+
+
 const Header = () => {
+
+const [selected, setSelected] = useState([]);
+
 return (
     <Container>
         <Row xs={1}>
@@ -31,13 +50,20 @@ return (
                 <Nav.Item>
                     <Nav.Link eventKey="link-3">Ranking</Nav.Link>
                 </Nav.Item>
+                <Typeahead
+                    id="basic-example"
+                    onChange={setSelected}
+                    options={options}
+                    placeholder="Choose a state..."
+                    selected={selected}
+                />
                 <Form className="d-flex">
-                    <Form.Control
+                    {/* <Form.Control
                         type="search"
                         placeholder="search"
                         className="me-2"
                         aria-label="Search"
-                    />
+                    /> */}
                     <Button variant="outline-sucess">User</Button>
                     <Button variant="outline-sucess">Carrito</Button>
                 </Form>
